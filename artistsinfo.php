1e36f1fbb80d528c7698a321369edc34b04e8e96
Yoichiro Goto <Yoichiro Goto>
2017-05-01 18:08:37 -0400

    database connection completed

--- artistsinfo.php
+++ artistsinfo.php
@@ -2,6 +2,35 @@
 
 <html>
 <body>
+<?php
+	//include("includes/header.php");
+?>
+
+<h1>Fred Zappia</h1>
+
+<?php
+	require_once 'includes/config.php';
+	$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+	if ($mysqli->errno) {
+		print($mysqli->error);
+		exit("");
+	}
+	$sql = "SELECT * FROM Artist INNER JOIN Address_artist ON Artist.zip=Address_artist.zip WHERE aid = 1;";
+	$result = $mysqli->query($sql);
+	if (!$result) {
+		print($mysqli->error);
+		exit("");
+	}
+	$row = $result->fetch_assoc();
+	print ($row['image']);
+	print ($row['phone']);
+	print ($row['email']);
+	print ($row['introduction']);
+	print ($row['street']);
+	print ($row['zip']);
+	print ($row['city']);
+	print ($row['state']);
+?>
 
 </body>
 </html>
