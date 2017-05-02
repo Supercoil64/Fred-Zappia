4c701a0407be37936633f26ad6aac2dd985957f5
Supercoil64 <brc77@cornell.edu>
2017-05-02 01:38:52 -0400

    Added contact form, not sending emails yet

--- contact.php
+++ contact.php
@@ -10,6 +10,19 @@
 	<body>
 	<?php include "includes/navbar.php" ?>
 	
+	<h1>Contact</h1>
+	
+	<form action="mailto:brc77@cornell.edu?subject=Message From Website" method="post" enctype="text/plain" id="contact_form">
+		<br>
+		<input type="text" name="name" id="mail" placeholder="Name"><br>
+		<br>
+		<input type="text" name="mail" id="mail" placeholder="E-Mail"><br>
+		<br>
+		<textarea type="text" name="comment" id="comment"></textarea><br><br>
+		<input type="submit" value="Send">
+		<input type="reset" value="Reset">
+	</form>
+	
 
 	</body>
 	
